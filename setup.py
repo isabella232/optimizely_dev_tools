425ea920d0662594b5c9d618de9c23f5a755f73a
Lucas Swartsenburg <lucas@optimizely.com>
2016-03-14 00:03:27 -0700

    Adding the Optimizely platform package

--- setup.py
+++ setup.py
@@ -4,13 +4,21 @@ setup(name='optimizely_dev_tools',
       version='0.0.1',
       description='Optimizely developer tools',
       url='http://developers.optimizely.com/',
-      author='Lucas Swartsenburg',
+      author='Jon Gaulding, Tyler Jones, Peng-Wen Chen, Ali Rizvi, Lucas Swartsenburg',
       author_email='lucas@optimizely.com',
       license='MIT',
-      packages=['optimizely_dev_tools'],
+      packages=['optimizely_dev_tools', 'optimizely_platform'],
       scripts=['opti'],
       install_requires=[
         'flask', 'colorama', 'PyYAML'
       ],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+      ],      
       package_data={'optimizely_dev_tools': ['assets/*.png', 'assets/*.csv', 'assets/*.rst/', 'assets/*.txt']},
       zip_safe=False)
