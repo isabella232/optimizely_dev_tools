0f60917b947a91fa9d2a42f5f7de89057654049b
Vignesh Raja <vignesh@optimizely.com>
2016-03-29 14:50:03 -0700

    Rename template_files to integration_package_files to remove ambiguity with html templates

--- optimizely_dev_tools/packagefiles.py
+++ optimizely_dev_tools/packagefiles.py
@@ -15,11 +15,11 @@ def create_config_file(package_name, default_config):
   file_name = 'config.yaml'
   file_content = ''
   if default_config:
-    resource_path = os.path.join('template_files', 'config_yaml_default_template')
+    resource_path = os.path.join('integration_package_files', 'config_yaml_default_template')
     file_content = pkg_resources.resource_string(resource_package, resource_path)
     file_content = file_content.format(package_name=package_name)
   else:
-    resource_path = os.path.join('template_files', 'config_yaml_unconfigured_template')
+    resource_path = os.path.join('integration_package_files', 'config_yaml_unconfigured_template')
     file_content = pkg_resources.resource_string(resource_package, resource_path)
   with open(os.path.join(package_name, file_name), 'w') as f:
     f.write(file_content)
@@ -28,11 +28,11 @@ def create_integration_file(package_name, default_config):
   file_name = 'integration.yaml'
   file_content = ''
   if default_config:
-    resource_path = os.path.join('template_files', 'integration_yaml_default_template')
+    resource_path = os.path.join('integration_package_files', 'integration_yaml_default_template')
     file_content = pkg_resources.resource_string(resource_package, resource_path)
     file_content = file_content.format(package_name=package_name)
   else:
-    resource_path = os.path.join('template_files', 'integration_yaml_unconfigured_template')
+    resource_path = os.path.join('integration_package_files', 'integration_yaml_unconfigured_template')
     file_content = pkg_resources.resource_string(resource_package, resource_path)
     file_content = file_content.format(package_name=package_name)
   with open(os.path.join(package_name, file_name), 'w') as f:
@@ -42,7 +42,7 @@ def create_example_response_file(package_name, default_config):
   file_name = 'example.json'
   file_content = '// Provide an example response from your streaming server (if you use dynamic audiences in your integration)'
   if default_config:
-    resource_path = os.path.join('template_files', 'example.json')
+    resource_path = os.path.join('integration_package_files', 'example.json')
     file_path = pkg_resources.resource_filename(resource_package, resource_path)
     copy(file_path, package_name)
   else:
@@ -53,11 +53,11 @@ def create_functions_js_file(package_name, default_config):
   file_name = 'functions.js'
   file_path = package_name + '/' + file_name
   if default_config:
-    resource_path = os.path.join('template_files', 'functions_default.js')
+    resource_path = os.path.join('integration_package_files', 'functions_default.js')
     template_path = pkg_resources.resource_filename(resource_package, resource_path)
     copyfile(template_path, file_path)
   else:
-    resource_path = os.path.join('template_files', 'functions_unconfigured.js')
+    resource_path = os.path.join('integration_package_files', 'functions_unconfigured.js')
     template_path = pkg_resources.resource_filename(resource_package, resource_path)
     copyfile(template_path, file_path)
 
@@ -65,7 +65,7 @@ def create_functions_js_test_file(package_name, default_config):
   file_name = 'functions_test.unittests.js'
   file_path = package_name + '/' + file_name
 
-  resource_path = os.path.join('template_files', 'functions_test_default.unittests.js')
+  resource_path = os.path.join('integration_package_files', 'functions_test_default.unittests.js')
   template_path = pkg_resources.resource_filename(resource_package, resource_path)
   copyfile(template_path, file_path)
 
@@ -73,11 +73,11 @@ def create_functions_py_file(package_name, default_config):
   file_name = 'functions.py'
   file_path = os.path.join(package_name, file_name)
   if default_config:
-    resource_path = os.path.join('template_files', 'functions_default.py')
+    resource_path = os.path.join('integration_package_files', 'functions_default.py')
     template_path = pkg_resources.resource_filename(resource_package, resource_path)
     copyfile(template_path, file_path)
   else:
-    resource_path = os.path.join('template_files', 'functions_unconfigured.py')
+    resource_path = os.path.join('integration_package_files', 'functions_unconfigured.py')
     template_path = pkg_resources.resource_filename(resource_package, resource_path)
     copyfile(template_path, file_path)
 
@@ -85,7 +85,7 @@ def create_functions_py_test_file(package_name, default_config):
   file_name = 'functions_test.py'
   file_path = package_name + '/' + file_name
 
-  resource_path = os.path.join('template_files', 'functions_test_default.py')
+  resource_path = os.path.join('integration_package_files', 'functions_test_default.py')
   template_path = pkg_resources.resource_filename(resource_package, resource_path)
   copyfile(template_path, file_path)
 
--- optimizely_dev_tools/scripts/create_under_test_js_files.sh
+++ optimizely_dev_tools/scripts/create_under_test_js_files.sh
@@ -3,7 +3,6 @@
 # This script creates a modified version of every functions.js file, named functions.under_test.js, with
 # "module.exports = " prepended to the original file. Assigning the functions mapping to the
 # "module.exports = " variable accommodates testing of the functions.
-echo $1
 find $1 -name functions.js | grep -o '.*/' | while read functions_js_file_dir; do
   original_file_name="functions.js"
   under_test_file_name="functions.under_test.js"
