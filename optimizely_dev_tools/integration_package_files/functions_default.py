be175f76f6f4a281ae2b43384b5ea12b609725ce
Vignesh Raja <vignesh@optimizely.com>
2016-04-04 09:45:12 -0700

    Update functions_default.py and functions_test_default.py with Matt's changes

--- optimizely_dev_tools/integration_package_files/functions_default.py
+++ optimizely_dev_tools/integration_package_files/functions_default.py
@@ -3,17 +3,37 @@ import requests
 from optimizely_platform import exceptions
 from optimizely_platform import objects
 
-def get_dynamic_audience_conditions(integration_settings):
-  url = str(integration_settings['url'])
 
-  response = requests.get(url).json()
+def get_dynamic_audience_conditions(integration_settings):
+  AUDIENCE_OPTIONS_ENDPOINT = (
+    'https://integrations.optimizely.how/api/{0}/campaigns.json')
+
+  account_id = integration_settings['account_id']
+  request_url = AUDIENCE_OPTIONS_ENDPOINT.format(account_id)
+  response = requests.get(request_url).json()
+
+  # Build list of audience conditions
   audience_condition_options = []
 
   try:
-    for campaign in response:
-      audience_condition_options.append(
-          objects.AudienceConditionOption(campaign['campaignId'], campaign['name'].strip()))
+    for campaign in response['campaigns']:
+      condition = objects.AudienceConditionOption(campaign['identifier'], campaign['name'].strip())
+      audience_condition_options.append(condition)
 
-    return [objects.AudienceCondition('campaigns', audience_condition_options)]
-  except Exception as e:
-    raise e
+    return [objects.AudienceCondition('Sample Condition', audience_condition_options)]
+  except:
+    raise
+
+def validate_integration_settings(integration_settings):
+  try:
+    verify_account(integration_settings['account_id'])
+  except:
+    raise optimizely_platform.exceptions.IntegrationSettingsValidationError(
+        'Your Account ID appears to be invalid.')
+
+  return optimizely_platform.Configuration.get_integration_strings()
+
+def verify_account(account_id):
+  VERIFY_ACCOUNT_ENDPOINT = 'https://api.sample-app.com/verify/'
+
+  return requests.get(VERIFY_ACCOUNT_ENDPOINT + account_id).json()
